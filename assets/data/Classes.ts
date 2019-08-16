b2c381b47c7839953d30ab1a6e9c0c68bc412fcf
kaur3009 <52581294+kaur3009@users.noreply.github.com>
2019-08-16 09:58:29 -0400

    Add files via upload

---
+++ assets/data/Classes.ts
@@ -0,0 +1,6 @@
+import {ClassInfo} from '../../app/ClassInfo';
+
+export const MYCLASSES:ClassInfo[]=[
+    {cName:'SYST24444',room:'E103',dow:'Friday',tod:'11-2'},
+    {cName:'SYST35300',room:'G225',dow:'Tuesday',tod:'8-11'}
+];
